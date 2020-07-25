059ccf33dfdaaf04cce619f78a42be4c7698a27b
Ove Henrik Draugsvol <ove.hd91@gmail.com>
2020-07-25 19:57:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+// layout stuff
+const expressLayouts = require('express-ejs-layouts')
+app.use(expressLayouts)
+app.use(express.static('public'))   // PUBLIC STATIC FILES
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views') // __dirname = current directory
+app.set('layout', 'layouts/layout')
+
+// body parse
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+// hente routes
+const indexRouter = require('./routes/index')       // henter indexRoute fra index route (via modules.exports)
+
+// use routes
+app.use('/', indexRouter)           
+
+app.listen(process.env.PORT || 8080)
